3e2716cd0a5ffcb10022a9557096c3fabf56e75b
Chad Hanna <crh184@psu.edu>
2016-07-08 12:24:30 -0400

    update

--- waveform_common.py
+++ waveform_common.py
@@ -7,26 +7,28 @@ def parse():
 	parser = argparse.ArgumentParser(description="This program uses an approximation to the dynamics of general relativity to predict the gravitational wave strain signal from a merging black hole binary.")
 	parser.add_argument("--mass1", action="store", type=float, default=10.0, help="Mass 1 (Solar Masses): default 10.0")
 	parser.add_argument("--mass2", action="store", type=float, default=10.0, help="Mass 2 (Solar Masses): default 10.0")
-	parser.add_argument("--spin1", action="store", type=float, default=0, help="Spin 1 (dimensionless): default 0")
-	parser.add_argument("--spin2", action="store", type=float, default=0, help="Spin 2 (dimensionless): default 0")
+	#parser.add_argument("--spin1", action="store", type=float, default=0, help="Spin 1 (dimensionless): default 0")
+	#parser.add_argument("--spin2", action="store", type=float, default=0, help="Spin 2 (dimensionless): default 0")
 	parser.add_argument("--distance", action="store", type=float, default=300, help="Distance (Mpc): default 300")
 	parser.add_argument("--secret", action="store", type=int, default=0, help="Make a secret waveform by setting a positive integer, e.g., 1,2,3...")
 	args = parser.parse_args()
 	for arg in ("mass1", "mass2"):
 		if getattr(args,arg) < 5. or getattr(args,arg) > 30.:
-			raise ValueError("Please choose masses between 10 and 30 Solar Masses")
+			raise ValueError("Please choose masses between 5 and 30 Solar Masses")
 	for arg in ("spin1", "spin2"):
 		if getattr(args,arg) < -0.95 or getattr(args,arg) > 0.95:
 			raise ValueError("Please choose spins between -0.95 and 0.95")
 	if args.distance > 1000 or args.distance < 300:
 		raise ValueError("Please choose a distance between 300 and 1000 Mpc")
+	if arg.mass1 < arg.mass2:
+		raise ValueError("Please choose mass1 to be greater than or equal to mass2")
 
 	dt = 1. / 4096.
 
 	m1 = args.mass1
 	m2 = args.mass2
-	s1 = args.spin1
-	s2 = args.spin2
+	s1 = 0#args.spin1
+	s2 = 0#args.spin2
 	dist = args.distance
 
 	hplus, hcross = lalsim.SimInspiralChooseTDWaveform(0, dt, m1*lal.MSUN_SI, m2*lal.MSUN_SI, 0, 0, s1, 0, 0, s2, 40., 0, dist*1.e6 * lal.PC_SI, 0, 0, 0, None, None, 0, 8, lalsim.GetApproximantFromString(str("IMRPhenomD")))
